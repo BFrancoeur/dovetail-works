4ca881c304aab27347048b9ce92858b474772612
BFrancoeur <francoeurbj@gmail.com>
2026-05-02 08:37:29 -0400

    Use PAYLOAD_DB_PUSH env var to push schema on first deploy, remove migrator

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -29,6 +29,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI ?? '',
     },
+    push: process.env.PAYLOAD_DB_PUSH === 'true',
   }),
 
   editor: lexicalEditor({}),
